3f99b42435ec9caa0df6628b70ea8117d8d1a225
proudhAteR <158060444+proudhAteR@users.noreply.github.com>
2024-12-12 13:24:53 -0500

    feature: adding bonuses

--- src/main/java/city_cleaner/CityCleanerGame.java
+++ src/main/java/city_cleaner/CityCleanerGame.java
@@ -31,7 +31,7 @@ public class CityCleanerGame extends Game {
         initializeEntities();
         initializePlayer();
         initializeEnemies();
-        bonus = new Bonus(new Position(300,400), 10);
+        bonus = new Bonus(new Position(300, 400), 10);
         configureRenderingEngine();
         configureCamera();
     }
@@ -49,10 +49,12 @@ public class CityCleanerGame extends Game {
         if (GameConfig.debugMode()) {
             renderDebugInfos(canvas);
         }
+
         renderEntities(canvas);
         if (!player.findBonus(bonus)) {
             bonus.place(canvas);
         }
+
         if (!GameConfig.debugMode()) {
             canvas.applyShaders(player.getSight().getBounds());
         }
